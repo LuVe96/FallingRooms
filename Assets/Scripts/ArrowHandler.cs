c11b2cb4ead12957242db0e5191c1e21714c9203
LuVe96 <LukiVe16@gmail.com>
2020-10-31 13:56:09 +0100

    added goal arrow

---
+++ Assets/Scripts/ArrowHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowHandler : MonoBehaviour
+{
+
+    public Transform goalObject;
+    void Update()
+    {
+        transform.LookAt(new Vector3(goalObject.transform.position.x, 3, goalObject.transform.position.z));
+
+    }
+}
