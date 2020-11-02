0680644a5b4f8ea96b1f76ed2abe7723373b54cc
LuVe96 <LukiVe16@gmail.com>
2020-11-02 17:29:56 +0100

    added key mechanics

---
+++ Assets/EndDoorHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndDoorHandler : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("Player").GetComponent<PlayerHandler>().currentKeysCount 
+            >= GameObject.Find("Player").GetComponent<PlayerHandler>().requiredeKeysCount)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
