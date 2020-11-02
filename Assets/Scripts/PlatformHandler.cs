0100405150016ca48e0d3824215b68486421a1d4
LuVe96 <LukiVe16@gmail.com>
2020-11-02 16:12:30 +0100

    consistent names, no random platformdrop

--- Assets/Scripts/PlatformHandler.cs
+++ Assets/Scripts/PlatformHandler.cs
@@ -5,15 +5,73 @@ using UnityEngine;
 public class PlatformHandler : MonoBehaviour
 {
 
-    // Start is called before the first frame update
-    void Start()
+    public float riseVelocity = 2;
+    private float timeSum = 0;
+    private float random;
+    public Material stdMaterial;
+    public Material redMaterial;
+    public float warningTime = 2;
+    private bool warnMaterialIsActive = false;
+
+    public float frequenzTime = 0.5f;
+    private float frequenzTimeSum = 0;
+
+
+    private void Start()
     {
-        
+        random = UnityEngine.Random.Range(5, 8);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (gameObject.transform.Find("PlatformElevateTrigger").GetComponent<PlayerEnterTrigger>().triggered)
+        {
+            if (transform.position.y < 0)
+            {
+                transform.position += new Vector3(0, riseVelocity * Time.deltaTime, 0);
+                if (transform.position.y >= 0)
+                {
+                    transform.position += new Vector3(0, -transform.position.y, 0);
+                }
+
+            }
+        }
+
+        if (gameObject.transform.Find("PlatformDropTrigger").GetComponent<PlayerEnterTrigger>().triggered)
+        {
+            timeSum += Time.deltaTime;
+
+            if (timeSum >= (random - warningTime))
+            {
+                frequenzTimeSum += Time.deltaTime;
+                if (frequenzTimeSum >= frequenzTime)
+                {
+                    frequenzTimeSum = 0;
+                    ToggleMaterial();
+                }
+            }
+
+            // hexagon is falling
+            if (timeSum >= random)
+            {
+                gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            }
+        }
+
+    }
+
+    private void ToggleMaterial()
+    {
+
+        if (!warnMaterialIsActive)
+        {
+            warnMaterialIsActive = true;
+            GetComponentInChildren<MeshRenderer>().material = redMaterial;
+        }
+        else
+        {
+            warnMaterialIsActive = false;
+            GetComponentInChildren<MeshRenderer>().material = stdMaterial;
+        }
     }
 }
--- Assets/Scripts/PlayerEnterTrigger.cs
+++ Assets/Scripts/PlayerEnterTrigger.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CallerTrigger : MonoBehaviour
+public class PlayerEnterTrigger : MonoBehaviour
 {
 
     public bool triggered { get; private set; } = false;
