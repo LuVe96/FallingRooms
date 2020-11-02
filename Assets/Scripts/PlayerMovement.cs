eccbe96f33fa78400b627245b77be82f6b728409
LuVe96 <LukiVe16@gmail.com>
2020-11-02 15:23:52 +0100

    modify movement by using rigidbody velocity, add walls to hexagons
    froze roation in rigidbody

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,11 +9,13 @@ public class PlayerMovement : MonoBehaviour
     private PlayerInputActions playerInputActions;
     public float playerSpeed = 3f;
     public Transform playerModel;
+    private Rigidbody rigidbody;
 
 
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     private void OnEnable()
@@ -32,22 +34,28 @@ public class PlayerMovement : MonoBehaviour
         float playerSpeedWD = playerSpeed * Time.deltaTime;
         Vector2 movmentVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
+        Vector3 newRigidbodyVelocity = new Vector3(0, rigidbody.velocity.y, 0);
+
         if ( movmentVector.x <= -0.1)
         {
-            transform.position += new Vector3(-playerSpeedWD, 0, 0);
+            //transform.position += new Vector3(-playerSpeedWD, 0, 0);
+            newRigidbodyVelocity = new Vector3(-playerSpeed, rigidbody.velocity.y, newRigidbodyVelocity.z);
         }
         else if (movmentVector.x >= 0.1)
         {
-            transform.position += new Vector3(playerSpeedWD, 0, 0);
+            //transform.position += new Vector3(playerSpeedWD, 0, 0);
+            newRigidbodyVelocity = new Vector3(playerSpeed, rigidbody.velocity.y, newRigidbodyVelocity.z);
         }
 
         if (movmentVector.y <= -0.1)
         {
-            transform.position += new Vector3(0, 0, -playerSpeedWD);
+            //transform.position += new Vector3(0, 0, -playerSpeedWD);
+            newRigidbodyVelocity = new Vector3(newRigidbodyVelocity.x, rigidbody.velocity.y, -playerSpeed);
         }
         else if (movmentVector.y >= 0.1)
         {
-            transform.position += new Vector3(0, 0, playerSpeedWD);
+            //transform.position += new Vector3(0, 0, playerSpeedWD);
+            newRigidbodyVelocity = new Vector3(newRigidbodyVelocity.x, rigidbody.velocity.y, playerSpeed);
         }
 
         if (movmentVector.x != 0 || movmentVector.y != 0)
@@ -56,6 +64,8 @@ public class PlayerMovement : MonoBehaviour
             playerModel.transform.rotation = Quaternion.RotateTowards(playerModel.transform.rotation, rotation, 10f);
 
         }
+
+        rigidbody.velocity = newRigidbodyVelocity;
 
     }
 }
