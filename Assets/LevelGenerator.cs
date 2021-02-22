78aca2e76342f0da7759abd3d8fcc5813b7c2ba7
LuVe96 <LukiVe16@gmail.com>
2021-02-22 10:57:37 +0100

    adjusted level generator

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,29 +7,95 @@ public class LevelGenerator : MonoBehaviour
 {
 
     public GeneratedPlatform[] generatedPlatforms;
+    public Transform player;
     private Level currentLevel;
+
+    private Vector3 currPos;
+    private float delta_x = 8.6608f;
+    private float delta_z = 7.4978f;
+
+    private bool evenLine = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentLevel = DataSaver.loadData<Level>("currentLevel");
-        Debug.Log("Level:");
-        Debug.Log(currentLevel);
+        currPos = new Vector3(0, 0, 0);
+         currentLevel = DataSaver.loadData<Level>("currentLevel");
+        createLevel();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void createLevel()
     {
-        
+        string[] platformStrings = currentLevel.LevelString.Split(',');
+
+        foreach (var item in platformStrings)
+        {
+
+            foreach (var platform in generatedPlatforms)
+            {
+                if(item.Substring(0,3) == platform.key.ToString())
+                {
+                    GameObject pf = Instantiate(platform.plattform);
+                    platform.plattform.transform.position = currPos;
+                    platform.plattform.transform.rotation = Quaternion.Euler(-90,0,0) ;
+
+                    if (item.Substring(0, 3) == PlatformKey.SSP.ToString())
+                    {
+                        player.transform.position += currPos;
+                    }
+
+                    switch (item.Substring(3, 1))
+                    {
+                        case "0": platform.plattform.transform.rotation = Quaternion.Euler(-90, 60 * 0f, 0); break;
+                        case "1": platform.plattform.transform.rotation = Quaternion.Euler(-90, 60 * 1f, 0) ; break;
+                        case "2": platform.plattform.transform.rotation = Quaternion.Euler(-90, 60 * 2f, 0); break;
+                        case "3": platform.plattform.transform.rotation = Quaternion.Euler(-90, 60 * 3f, 0); break;
+                        case "4": platform.plattform.transform.rotation = Quaternion.Euler(-90, 60 * 4f, 0); break;
+                        case "5": platform.plattform.transform.rotation = Quaternion.Euler(-90, 60 * 5f, 0); break;
+                        default:
+                            break;
+                    }
+
+                    currPos += new Vector3(delta_x, 0, 0);
+                    
+                }
+            }
+
+            switch (item)
+            {
+                case "brea":
+                    evenLine = !evenLine;
+                    if (evenLine)
+                    {
+                        currPos = new Vector3(0, 0, currPos.z - delta_z);
+                    } else
+                    {
+                        currPos = new Vector3(-delta_x/2, 0, currPos.z - delta_z);
+                    }
+                    break;
+                case "_00_": currPos += new Vector3(delta_x, 0, 0); break;
+                case "empty":
+                    break;
+                case "end":
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
     }
-
-
 
 }
 
 [System.Serializable]
 public class GeneratedPlatform
 {
-    public string key;
+    public PlatformKey key;
     public GameObject plattform;
 }
+
+    public enum PlatformKey
+    {
+        SSP, SNP, SGP, _00, empt, br, end
+    }
